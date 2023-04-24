053c2ac285a880304c0e1eaa998a3c0587c31a86
Rohan Tikotekar <87898751+rohantikotekar@users.noreply.github.com>
2023-04-24 21:47:59 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -10,7 +10,7 @@ class WebsiteLoadTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200, 'Website did not load properly.')
 
         if response.status_code == 200:
-            print("Website is running. Hello World.")
+            print("Website is running.")
 
 if __name__ == '__main__':
     unittest.main()
